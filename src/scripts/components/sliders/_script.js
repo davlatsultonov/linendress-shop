c761e92b45ed671b0581c90b1231f3429dcab6f1
davlatsultonov <davlatsultonov2001@gmail.com>
2020-08-13 18:22:51 +0500

    [ GLOBAL ] добавлены подложки
    также поправлены стили блоков

--- src/scripts/components/sliders/_script.js
+++ src/scripts/components/sliders/_script.js
@@ -2,10 +2,18 @@ $(document).ready(function() {
     $('.info-cards').owlCarousel({
         items: 1,
         margin: 40,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
     });
 
     $('.product').owlCarousel({
         items: 1,
         margin: 40,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true
     });
 });
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,12 @@
 import './components/lang-switcher/_script';
 import './components/nav-menu/_script';
 import './components/sliders/_script';
+
+$(window).on('load', function() {
+    $('.info-block').each(function () {
+        let totalHeight = $(this).find('.info-block__img').css('height') + $(this).find('.info-block__statement').css('height');
+        $(this).children('.substrate__item').height(totalHeight);
+    });
+
+    $('.header').find('.substrate__item').height($('.header').outerHeight() + 105);
+});
