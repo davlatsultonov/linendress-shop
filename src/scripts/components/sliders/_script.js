b2b0fe0656b717492fb15d9e628aba05f02cad6d
davlatsultonov <davlatsultonov2001@gmail.com>
2020-08-26 00:26:59 +0500

    [ PRODUCT-VIEW ] добавлен блок для обзора продукта

--- src/scripts/components/sliders/_script.js
+++ src/scripts/components/sliders/_script.js
@@ -19,4 +19,14 @@ $(document).ready(function() {
         margin: 40,
         loop: true
     });
+
+    $('#product-view__gallery').owlCarousel({
+        items: 1,
+        margin: 40,
+        loop: true,
+        dots: false,
+        smartSpeed :900,
+        nav: true,
+        navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+    });
 });
