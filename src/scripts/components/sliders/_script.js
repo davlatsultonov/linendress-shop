537f1c3f3c7ba836e6e09741776eff48523ffa5e
davlatsultonov <davlatsultonov2001@gmail.com>
2020-08-26 23:37:16 +0500

    [ GLOBAL ] fix

--- src/scripts/components/sliders/_script.js
+++ src/scripts/components/sliders/_script.js
@@ -1,32 +1,30 @@
-$(document).ready(function() {
-    $('.info-cards').owlCarousel({
-        items: 1,
-        margin: 40,
-        loop: true,
-        autoplay: true,
-        autoplayTimeout:3000,
-        autoplayHoverPause:true,
-    });
+$('.info-cards').owlCarousel({
+    items: 1,
+    margin: 40,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+});
 
-    $('#products-group').owlCarousel({
-        items: 1,
-        margin: 40,
-        loop: true
-    });
+$('#products-group').owlCarousel({
+    items: 1,
+    margin: 40,
+    loop: true
+});
 
-    $('.product-card__gallery').owlCarousel({
-        items: 1,
-        margin: 40,
-        loop: true
-    });
+$('.product-card__gallery').owlCarousel({
+    items: 1,
+    margin: 40,
+    loop: true
+});
 
-    $('#product-view__gallery').owlCarousel({
-        items: 1,
-        margin: 40,
-        loop: true,
-        dots: false,
-        smartSpeed :900,
-        nav: true,
-        navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
-    });
+$('#product-view__gallery').owlCarousel({
+    items: 1,
+    margin: 40,
+    loop: true,
+    dots: false,
+    smartSpeed :900,
+    nav: true,
+    navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
 });
